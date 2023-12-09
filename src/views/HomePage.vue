4cb6cdf80f7d1c8ab76c73e4283d2f2aa38a97fa
Victor25Ortiz <143905805+Victor25Ortiz@users.noreply.github.com>
2023-12-09 01:05:17 -0600

    fixed categories not appearing

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,60 +1,56 @@
 <script setup>
-import { onMounted, ref } from 'vue'
+import { onMounted, ref } from 'vue';
 import useAPI from '@/composables/useAPI'
-
-const api = useAPI()
-const categories = ref([])
+import BaseTitle from '@/components/BaseTitle.vue'
+const {categories, getCategories} = useAPI()
 
 onMounted(async () => {
-  categories.value = await api.getCategories()
+  
+    await getCategories()
+
 })
 </script>
 
-
 <template>
-  <div class="brand">
-    <img class="logo" src="logo.svg" alt="logo" />
-    <h1 class="title">Trivia-app</h1>
-    <img class="logo" src="logo.svg" alt="logo" />
-  </div>
-  <div class="categories">
-    <RouterLink
-    v-for="category in categories"
-    :key="category.id"
-    :to="`/question/category/${category.id}`"
-    class="category"
-  >
+  <BaseTitle>
+    <template #logo>
+      <img src="/logo.svg" alt="" />
+    </template>
+    Triviantastic
+    
+  </BaseTitle>
+  <div v-if="categories.length > 0" class="categories">
+    <RouterLink :to="`/question/category/${category.id}`" v-for="category in categories" :key="category.id" class="category">
       {{ category.name }}
     </RouterLink>
   </div>
 </template>
 
-<style lang="postcss" scoped>
+<style lang ="postcss" scoped>
 .brand {
-      @apply flex items-center justify-center gap-4;
+  @apply flex items-center justify-center gap-4;
 
-      & .logo {
-            @apply h-16 w-16;
-           
-            & .title {
-                  @apply text-6xl font-thin uppercase tracking-widest text-slate-700;
-            }
-      }
+  & .logo {
+    @apply h-16 w-16;
+  }
+
+  & .title {
+    @apply text-6xl font-thin tracking-widest text-slate-700 uppercase;
+  }
 }
 
 .categories {
-      @apply grid flex-grow grid-cols-4 gap-12;
+  @apply grid flex-grow grid-cols-4 gap-12;
 
-      & .category{
-            @apply flex h-32 items-center justify-center rounded-lg border-4 border-green-500 py-4 font-bold uppercase text-slate-600 transition-colors duration-300;
+  & .category {
+    @apply text-center flex h-32 items-center justify-center rounded-lg border-4 border-slate-500 py-4 font-bold uppercase transition-colors duration-300;
 
-            &:hover {
-                  @apply cursor-pointer border-red-500 bg-red-500 text-white;
-            }
+      &:hover {
+        @apply cursor-pointer border-red-500 bg-red-500 text-white;
       }
+  }
+
+
 }
 
-.score {
-  @apply absolute bottom-8 right-8 text-2xl font-bold uppercase text-slate-700;
-}
 </style>
--- src/views/QuestionPage.vue
+++ src/views/QuestionPage.vue
@@ -1,12 +1,15 @@
 <script setup>
 import { onMounted, ref } from 'vue'
 import { useRoute, useRouter } from 'vue-router'
+import { shuffle } from 'lodash-es'
 
 import useAPI from '@/composables/useAPI'
 import useColor from '@/composables/useColor'
 import useScore from '@/composables/useScore';
 import BaseTitle from '@/components/BaseTitle.vue'
-import DifficultyChip from '@/DifficultyChip.vue';
+import DifficultyChip from '@/components/DifficultyChip.vue'
+import NotificationAnswers from '@/components/NotificationAnswers.vue'
+
 
 const route = useRoute()
 const router = useRouter()
@@ -15,69 +18,78 @@ const api = useAPI()
 const question = ref(null)
 const answers = ref([])
 
-const { changeScore } = useScore()
+const showNotification = ref(false)
+const isCorrect = ref(false)
+
+const {changeScore} = useScore()
+
 const handleAnswer = (points) => {
+  isCorrect.value = points > 0
+  showNotification.value = true
+  setTimeout(() => {
     changeScore(points)
-    router.push('/')
+    router.push('/triviantastic/')
+  }, 1000)
+
 }
+
 onMounted(async () => {
-    question.value = await api.getQuestion(route.params.id)
+  question.value = await api.getQuestion(route.params.id)
+  answers.value.push({
+    id: answers.value.length,
+    correct: true,
+    answer: question.value.correct_answer,
+    points: question.value.difficulty === 'easy' ? 10 : question.value.difficulty === 'medium' ? 20 : 30,
+  })
+  question.value.incorrect_answers.map((answer) => {
     answers.value.push({
-        id: answers.value.length,
-        correct: true,
-        answer: question.value.correct_answer,
-        points: question.value.difficulty == 'easy' ? 10 :
-                question.value.difficulty == 'medium' ? 20 : 30
+      id: answers.value.length,
+      correct: false,
+      answer,
+      points: -5,
     })
-    question.value.incorrect_answers.map((answer) => {
-        answers.value.push({
-            id: answers.value.length,
-            correct: false,
-            answer,
-            points: -5,
-        })
-    })
+  })
+
+  answers.value = shuffle(answers.value)
+
 })
 </script>
 
 <template>
-    <div v-if="question" class="question-container">
+  <div v-if="question" class="question-container">
     <BaseTitle> {{ question.category }}</BaseTitle>
-    <p class="question">{{ question.question }}</p>
+    <div v-html="question.question" class="question"></div>
     <div class="answers">
-        <div v-for="answer in answers" :key="answer.id" :class="colors.getColor(answer.id)" class="answer">
-            <div @click="handleAnswer(answer.points)">
-            {{ answer.answer }}
-        </div>
-        </div>
+      <div v-html="answer.answer" v-for="answer in answers" :key="answer.id" :class="colors.getColor(answer.id)" class="answer" @click="handleAnswer(answer.points)">
+      </div>
     </div>
     <DifficultyChip :difficulty="question.difficulty"/>
-    </div>
-    <div v-else class="loading">Loading ...</div>
+  </div>
+  <div v-else class="loading">Loading...</div>
+  <NotificationAnswers :correct="isCorrect" v-if="showNotification"/>
 </template>
 
 <style lang="postcss" scoped>
 .question-container {
-    @apply flex h-full w-full flex-col items-center gap-8;
+  @apply flex h-full w-full flex-col items-center gap-8 p-10;
 
-& .question {
+  & .question {
     @apply text-center text-2xl font-bold;
-}
+  }
 
-& .answers {
+  & .answers {
     @apply grid w-full flex-grow grid-cols-2 gap-8;
-}
-
-& .answer {
-    @apply flex items-center justify-center rounded-lg text-center text-4xl text-white;
     &:hover {
-        @apply cursor-pointer;
+      @apply cursor-pointer;
     }
-}
+
+    & .answer {
+      @apply flex items-center justify-center rounded-lg text-center text-4xl text-white;
+    }
+  }
 }
 
-loading {
-    @apply flex h-full w-full items-center justify-center text-7xl;
+.loading {
+  @apply flex h-full w-full items-center justify-center text-7xl;
 }
 </style>
-
