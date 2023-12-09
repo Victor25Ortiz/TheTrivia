ed976c123c23ac2b52e0101743fe2052365856f4
Victor25Ortiz <143905805+Victor25Ortiz@users.noreply.github.com>
2023-12-09 00:47:39 -0600

    fixed categories

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -46,7 +46,7 @@ onMounted(async () => {
       @apply grid flex-grow grid-cols-4 gap-12;
 
       & .category{
-            @apply flex h-32 w-40 items-center justify-center rounded-lg border-4 border-green-500 py-4 font-bold uppercase text-slate-600 transition-colors duration-300;
+            @apply flex h-32 items-center justify-center rounded-lg border-4 border-green-500 py-4 font-bold uppercase text-slate-600 transition-colors duration-300;
 
             &:hover {
                   @apply cursor-pointer border-red-500 bg-red-500 text-white;
