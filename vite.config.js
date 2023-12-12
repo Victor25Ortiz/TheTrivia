0ac200def062b3b5b421270d3c9dcb5bf852856c
Victor25Ortiz <143905805+Victor25Ortiz@users.noreply.github.com>
2023-12-11 21:51:13 -0600

    upload

--- vite.config.js
+++ vite.config.js
@@ -3,12 +3,13 @@ import vue from '@vitejs/plugin-vue'
 import path from 'path'
 
 // https://vitejs.dev/config/
+
 export default defineConfig({
+  base: '/TheTrivia/',
   plugins: [vue()],
   resolve: {
-    alias: {
+  alias: {
       '@': path.resolve(__dirname, 'src'),
-      base: '/TheTrivia/'
     },
   },
 })
